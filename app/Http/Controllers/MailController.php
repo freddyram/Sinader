920c78d67c7a089f9a21be19ada179d8d0c51b4f
unknown <rene.maldonado@gmail.com>
2020-07-01 11:13:27 -0400

    agrega mail 1.0

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Mail;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller {
+  
+   public function html_email() {
+      $data = array('name'=>"Virat Gandhi");
+      Mail::send('mail', $data, function($message) {
+         $message->to('abc@gmail.com', 'Tutorials Point')->subject
+            ('Laravel HTML Testing Mail');
+         $message->from('xyz@gmail.com','Virat Gandhi');
+      });
+      echo "HTML Email Sent. Check your inbox.";
+   }
+
+}
